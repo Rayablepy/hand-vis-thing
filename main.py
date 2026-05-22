8d490e57fd28518a4542b0c3d03740eca281e44f
Rayable <rayable11.0808@gmail.com>
2026-05-22 23:09:54 +0800

    image formatting fixes

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 def result_output(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print(f"Result: {result}")
-
+    print("-"*30)
+    #time.sleep(1)
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='./resources/hand_landmarker.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=result_output)
-
 with HandLandmarker.create_from_options(options) as landmarker:
     cap = cv.VideoCapture(0)
-
     while True:
         ret, frame = cap.read()
         if not ret:
+            print("System error")
             break
+        frame = cv.flip(frame, 1)
         timestamp_ms = int(time.time() * 1000)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame,cv.COLOR_BGR2RGB))
         landmarker.detect_async(mp_image, timestamp_ms=timestamp_ms)
-
         cv.imshow('window', frame)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
