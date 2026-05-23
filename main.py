a6b75c2293d27ec06a088ed771c4a1647393f493
Rayable <rayable11.0808@gmail.com>
2026-05-23 23:43:07 +0800

    added gesture_recogniser model and changed related code and methods

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import numpy as np
 import cv2 as cv
+from mediapipe.tasks.python.vision.gesture_recognizer_result import GestureRecognizerResult
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -11,19 +14,21 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 mp_hands = mp.tasks.vision.HandLandmarksConnections
 mp_drawing = mp.tasks.vision.drawing_utils
 mp_styles = mp.tasks.vision.drawing_styles
+
 #styles below and drawframe function extracted from mediapipe colab notebook
 margin = 10
 font_size = 1
 font_thickness = 1
-txt_colour = (255,0,0)
+txt_colour = (0,255,0)
 latest_result = None
 
-def drawframe(currframe, result: HandLandmarkerResult | None):
+def drawframe(currframe, result: GestureRecognizerResult | None):
     if result is None:
         return currframe
 
     landmarks_list = result.hand_landmarks
     handedness_list = result.handedness
+    gestures_list = result.gestures
     mod_frame = np.copy(currframe)
 
     for i in range(len(landmarks_list)):
@@ -42,19 +47,33 @@ def drawframe(currframe, result: HandLandmarkerResult | None):
         y_coords = [landmark.y for landmark in landmarks]
         txt_x = int(min(x_coords)*width)
         txt_y = int(min(y_coords)*height) - margin
+        hand_label = handedness[0].category_name
+        gesture_label = ""
+        if i<len(gestures_list) and gestures_list[i]:
+            top_gesture = gestures_list[i][0]
+            gesture_label = f"{top_gesture.category_name} {top_gesture.score}"
+        full_label = f"{hand_label}: {gesture_label}" if gesture_label else hand_label
 
-        cv.putText(mod_frame,f"{handedness[0].category_name}",(txt_x,txt_y),cv.FONT_HERSHEY_DUPLEX,font_size,txt_colour,font_thickness,cv.LINE_AA)
+        cv.putText(mod_frame,full_label,(txt_x,txt_y),cv.FONT_ITALIC,font_size,txt_colour,font_thickness,cv.LINE_AA)
     return mod_frame
 
-def result_output(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+def result_output(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     global latest_result
     latest_result = result
 
-options = HandLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path='./resources/hand_landmarker.task'),
+base_options = python.BaseOptions(model_asset_path='./resources/gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(
+    base_options=base_options,
     running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=result_output)
-with HandLandmarker.create_from_options(options) as landmarker:
+    result_callback=result_output
+                                          )
+
+'''options = HandLandmarkerOptions(
+    base_options=python.BaseOptions(model_asset_path='./resources/hand_landmarker.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=result_output)'''
+
+with vision.GestureRecognizer.create_from_options(options) as landmarker:
     cap = cv.VideoCapture(0)
     while True:
         ret, frame = cap.read()
@@ -64,7 +83,7 @@ with HandLandmarker.create_from_options(options) as landmarker:
 
         timestamp_ms = int(time.time() * 1000)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv.cvtColor(frame,cv.COLOR_BGR2RGB))
-        landmarker.detect_async(mp_image, timestamp_ms=timestamp_ms)
+        landmarker.recognize_async(mp_image, timestamp_ms=timestamp_ms)
         annotated = drawframe(frame.copy(), latest_result)
         cv.imshow('window', annotated)
         if cv.waitKey(1) & 0xFF == ord('q'):
